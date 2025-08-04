ac623709c6c2f38ab56b916b4f37ff5c3702b822
sohid7254 <sohidameen321@gmail.com>
2025-08-04 15:24:19 +0600

    things a little bit changed

--- problem3.js
+++ problem3.js
@@ -1,8 +1,8 @@
 /** Problem -03 ( Medicine Planner ) */
 var lastDay = 11;
 //write your code here
-console.log("Output-(string)");
-for (var serial = 1; serial <= lastDay; serial++){
+
+for ( serial = 1; serial <= lastDay; serial++){
     if (serial % 3 === 0){
         console.log(serial,"-medicine" );
     }else{
